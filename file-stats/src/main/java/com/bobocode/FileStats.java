2d9fe0e64dd0b5c66e45115ec94454be57f703e1
KirillAnastasiev <kirillanastasiev1986@gmail.com>
2025-06-21 16:18:04 +0300

    file-stats: FileStats.from() done

--- file-stats/src/main/java/com/bobocode/FileStats.java
+++ file-stats/src/main/java/com/bobocode/FileStats.java
@@ -1,10 +1,44 @@
 package com.bobocode;
 
+import javax.print.URIException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 /**
  * {@link FileStats} provides an API that allow to get character statistic based on text file. All whitespace characters
  * are ignored.
  */
 public class FileStats {
+
+    private FileStats(String fileName) throws FileStatsException {
+        if (Objects.isNull(fileName)) {
+            throw new FileStatsException("Argument 'fileName' is null");
+        }
+
+        Path path = createPathFromFileName(fileName);
+        if (Files.notExists(path)) {
+            throw new FileStatsException("File '" + fileName + "' not exists");
+        }
+    }
+
+    private Path createPathFromFileName(String fileName) throws FileStatsException {
+        try {
+            URL fileURL = FileStats.class.getClassLoader().getResource(fileName);
+            if (Objects.isNull(fileURL)) {
+                throw new FileStatsException("Cannot find file '" + fileName + "'");
+            }
+
+            return Paths.get(fileURL.toURI());
+        } catch (URISyntaxException e) {
+            throw new FileStatsException("Cannot open file: '" + fileName + "'", e);
+        }
+    }
+
     /**
      * Creates a new immutable {@link FileStats} objects using data from text file received as a parameter.
      *
@@ -12,7 +46,7 @@ public class FileStats {
      * @return new FileStats object created from text file
      */
     public static FileStats from(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        return new FileStats(fileName);
     }
 
     /**
